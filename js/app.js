e60ad275dc8ae55df58b6d3266ea626ba9b0f0a0
Maxence <levesqu221@campus.unicaen.fr>
2023-09-11 14:57:55 +0200

    fin de cours

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let tempsInitial = 25;
+let tempsInitial = 24;
 let tempsPause = 5;
-let secondeInitial=0;
+let secondeInitial=59;
 
 let label = document.querySelectorAll("p");
 let boutonLancer = document.getElementById('lancer');
@@ -9,21 +9,21 @@ let boutonRedem = document.getElementById('redem');
 boutonRedem.style.display='none';
 
 
-document.getElementById("chrono").innerHTML=`${tempsInitial}:${secondeInitial}`;
-
-
 function lancer(){
     boutonLancer.style.display='none';
     boutonRedem.style.display='block' 
     label[0].style.color="lightgoldenrodyellow";
-    while(tempsInitial!=0){
-        for(secondeInitial=60;secondeInitial!=0;secondeInitial--){
-            if(i==0){
-                tempsInitial--;
-            }
+
+    while(!(tempsInitial==0)){
+        while(secondeInitial!=0){
+            secondeInitial--;
             document.getElementById("chrono").innerHTML=`${tempsInitial} : ${secondeInitial}`;
-         }
+            console.log(`${tempsInitial} : ${secondeInitial}`);
+        }
+        secondeInitial=59;
+        tempsInitial--;
     }
+
     label[0].style.color="whitesmoke";
     label[1].style.color="lightgoldenrodyellow";
 }
@@ -31,7 +31,6 @@ function lancer(){
 function redem(){
     boutonRedem.style.display='none';
     boutonLancer.style.display='block'
-    label[1].style.color="lightgoldenrodyellow"
 }
 
 
