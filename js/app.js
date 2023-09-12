d01fc2a07fb89a62e9127bfc79e71bfa73902c67
Maxence <levesqu221@campus.unicaen.fr>
2023-09-12 11:57:28 +0200

    chronomètre en place

--- js/app.js
+++ js/app.js
@@ -1,36 +1,61 @@
-let tempsInitial = 24;
+let tempsInitial = 25;
 let tempsPause = 5;
-let secondeInitial=59;
+let secondePause = 0;
+let secondeInitial = 0;
 
 let label = document.querySelectorAll("p");
 let boutonLancer = document.getElementById('lancer');
 let boutonRedem = document.getElementById('redem');
 
-boutonRedem.style.display='none';
+boutonRedem.style.display = 'none';
 
-
-function lancer(){
-    boutonLancer.style.display='none';
-    boutonRedem.style.display='block' 
-    label[0].style.color="lightgoldenrodyellow";
-
-    while(!(tempsInitial==0)){
-        while(secondeInitial!=0){
-            secondeInitial--;
-            document.getElementById("chrono").innerHTML=`${tempsInitial} : ${secondeInitial}`;
-            console.log(`${tempsInitial} : ${secondeInitial}`);
-        }
-        secondeInitial=59;
-        tempsInitial--;
+function affichage(chrono){
+    let temps = chrono.toString();
+    if(temps.length<2){
+        temps = '0'+temps;
     }
-
-    label[0].style.color="whitesmoke";
-    label[1].style.color="lightgoldenrodyellow";
+    return temps;
 }
 
-function redem(){
-    boutonRedem.style.display='none';
-    boutonLancer.style.display='block'
+function test(){
+    if (!(tempsInitial <=0)) {
+        if (!(secondeInitial <=0)) {
+            document.getElementById("chrono").innerHTML = `${affichage(tempsInitial)} : ${affichage(secondeInitial)}`;
+            secondeInitial--;
+        }
+        if(secondeInitial==0){
+            secondeInitial = 59;
+            tempsInitial--;
+        } 
+    }
+    pause=true;
+}
+
+function lancer() {
+
+    let pause = false;
+
+    boutonLancer.style.display = 'none';
+    boutonRedem.style.display = 'block';
+
+    label[0].style.color = "lightgoldenrodyellow";
+
+    setInterval(test,1000);
+
+    if (pause==true){
+        label[0].style.color = "whitesmoke";
+        label[1].style.color = "lightgoldenrodyellow";
+    }
+    
+}
+
+function redem() {
+    boutonRedem.style.display = 'none';
+    boutonLancer.style.display = 'block'
+}
+
+function load(){
+    location.reload();
 }
 
 
